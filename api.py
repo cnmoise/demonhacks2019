e81fc3ed33b1d583c5ef3ee0139a0eca2198c2f3
Adam <me@adamburns.org>
2018-10-20 22:35:36 -0500

    Edited errors in the get_recipes() function, needs a new key

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 import requests
-from clarifai.rest import ClarifaiApp
+#from clarifai.rest import ClarifaiApp
 
-app = ClarifaiApp(api_key=get_api_key(keys/clarifai.key))
-app = ClarifaiApp()
+#app = ClarifaiApp(api_key=get_api_key('keys/clarifai.key'))
+#app = ClarifaiApp()
 
 
 def get_api_key(filename):
@@ -10,7 +10,7 @@ def get_api_key(filename):
         with open(filename, 'r') as f:
             return f.read().strip()
     except FileNotFoundError:
-        print''%s' file not found' % filename)
+        print(filename + ' file not found')
 
 
 def get_ingredients(image):
@@ -21,18 +21,30 @@ def get_ingredients(image):
 def get_recipes(ingredient_list):
     MAX_RESULTS = 5
     DELIMETER = '%2C'
-    MASHAPE_KEY = get_api_key(keys/mashape.key)
+    MASHAPE_KEY = get_api_key('keys/mashape.key')
     PREFIX_URL = (
         'https://spoonacular-recipe-food-nutrition-v1.p.mashape.'
         'com/recipes/findByIngredients?fillIngredients=false&'
         'ingredients=')
     SUFFIX_URL = (
         '&limitLicense=false&number=' + 
-        MAX_RESULTS + 
+        str(MAX_RESULTS) + 
         '&ranking=1')
     ingredients = DELIMETER.join(ingredient_list)
     url = PREFIX_URL + ingredients + SUFFIX_URL
+    print(ingredients)
+    print(url)
     headers = {
         'X-Mashape-Key': MASHAPE_KEY, 
         'Accept': 'application/json'}
-    return reqests.get(url, headers)
+    print(headers)
+    return requests.get(url, headers=headers)
+
+
+def main():
+    INGREDIENTS = ['apple', 'flour', 'sugar']
+    print(get_recipes(INGREDIENTS))
+
+
+if __name__ == '__main__':
+    main()
